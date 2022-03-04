122619835cba43fe4bb3807ced1e547dcecc965f
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2022-03-04 02:00:15 +0100

    Updated in pest syntax

--- src/Cart/Tests/Feature/CartTest.php
+++ src/Cart/Tests/Feature/CartTest.php
@@ -1,70 +1,68 @@
 <?php
 
-namespace Tests\Feature;
-
-use Tests\TestCase;
 use PaltaSolutions\Cart\Domain\Models\Cart;
 use PaltaSolutions\Cart\Domain\Models\CartItem;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class CartTest extends TestCase
-{
-    // use RefreshDatabase;
+use function PHPUnit\Framework\assertCount;
 
-    public function testQueriesCart(): void
-    {
-        $cart = Cart::factory()->create();
+uses(Tests\TestCase::class);
 
-        $response = $this->graphQL(/** @lang GraphQL */'
-        query ($id: String!){
+it('can query existing cart', function () {
+    $cart = Cart::factory()->create();
+
+    $gql = <<<'GQL'
+        query ($id: String!) {
             cart(id: $id) {
                 id
                 email
             }
         }
-        ', [
-            'id' => $cart->id,
-        ]);
+GQL;
 
-        $response->assertJson([
-            'data' => [
-                'cart' => [
-                    'id' => $cart->id,
-                    'email' => $cart->email
-                ]
+    graphQL($gql, [
+        'id' => $cart->id,
+    ])
+    ->assertJson([
+        'data' => [
+            'cart' => [
+                'id' => $cart->id,
+                'email' => $cart->email
             ]
-        ]);
-    }
+        ]
+    ]);
+});
 
-    public function testAddItemToCart(): void
-    {
-        $cart = Cart::factory()->create();
-        // $cartItem = $cart->items()->make();
-        $cartItem = CartItem::factory()->make();
-        $this->assertCount(0, $cart->items);
-        $this->graphQL(/** @lang GraphQL */'
-            mutation($cart_id: String!, $name: String, $unit_total_amount: Int!) {
-                addItemToCart(
-                  input: {
+it('can add an item to cart', function(){
+    $cart = Cart::factory()->create();
+    $cartItem = CartItem::factory()->make();
+
+    assertCount(0, $cart->items);
+
+    $query = <<<'GQL'
+        mutation($cart_id: String!, $name: String, $unit_total_amount: Int!) {
+            addItemToCart(
+                input: {
                     cart_id: $cart_id
                     name: $name
                     unit_total_amount: $unit_total_amount
-                  }
-                ) {
-                  id
-                  email
-                  items {
+                }
+            ) {
+                id
+                email
+                items {
                     id
                     name
                     quantity
-                  }
                 }
-              }
-        ', [
-            'cart_id' => $cart->id,
-            'name' => $cartItem->name,
-            'unit_total_amount' => $cartItem->unit_total_amount
-        ])
+            }
+        }
+GQL;
+
+    graphQL($query, [
+        'cart_id' => $cart->id,
+        'name' => $cartItem->name,
+        'unit_total_amount' => $cartItem->unit_total_amount,
+    ])
         ->assertJson([
             'data' => [
                 'addItemToCart' => [
@@ -77,6 +75,6 @@ class CartTest extends TestCase
                 ]
             ]
         ]);
-        $this->assertCount(1, $cart->fresh(['items'])->items);
-    }
-}
+
+    assertCount(1, $cart->fresh(['items'])->items);
+});
